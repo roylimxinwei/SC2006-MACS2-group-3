806dfbb6bde6f428684368903d1d7605f0771a61
seahkahyen <kimeunhe111@gmail.com>
2024-03-08 19:18:12 +0800

    new

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,7 @@
 // Welcome screen links to sign up and log in screen
 
 
-import React, {useState, useEffect}  from "react";
-
+import React, { useState, useEffect } from "react";
 import {
   Dimensions,
   Image,
@@ -13,44 +12,74 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-
-
 import * as Location from "expo-location";
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import colors from "../config/colors";
-
+//import GlobalApi from "../config/GlobalApi";
 
 const HomeScreen = ({ navigation }) => {
   const [currentLocation, setCurrentLocation] = useState(null);
   const [initialRegion, setInitialRegion] = useState(null);
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [restaurants, setRestaurants] = useState([]);
 
-  useEffect (() => {
+  useEffect(() => {
     const getLocation = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         console.log("Permission to access location was denied");
         return;
       }
-
+    
       let location = await Location.getCurrentPositionAsync({});
       setCurrentLocation(location.coords);
-
+    
       setInitialRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       });
+      console.log("Initial Region: ", initialRegion);
     };
 
     getLocation();
-  }, []) ;
+    
+    // useEffect(()=>{
+    //   location&&GetNearbyLocation();
+    // },[location])
 
+    // const GetNearbyLocation = ()=>{
+    //   const data ={
+    //     "includedTypes": ["restaurant","bakery","cafe"],
+    //     "maxResultCount": 10,
+    //     "locationRestriction": {
+    //       "circle": {
+    //         "center": {
+    //           "latitude": location?.latitude,
+    //           "longitude": location?.longtitude},
+    //         "radius": 500.0
+    //   }}}
+    //   GlobalApi.NewNearByPlace(data).then(resp =>{
+    //     console.log(resp.data);
+    //   })
+    // }
+
+  //   const fetchRestaurants = async () => {
+  //     try {
+  //       const data = require("./restaurants.json");
+  //       console.log(data);
+  //       setRestaurants(data);
+  //     } catch (error) {
+  //       console.error("Error fetching restaurants:", error);
+  //     }
+  //   };
+
+  //   fetchRestaurants();
+  // }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Home Screen!</Text>
-
       {initialRegion && (
         <MapView style={styles.map} initialRegion={initialRegion}>
           {currentLocation && (
@@ -62,6 +91,16 @@ const HomeScreen = ({ navigation }) => {
               title="Your Location"
             />
           )}
+          {restaurants.map((restaurant, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: restaurant.latitude,
+                longitude: restaurant.longitude,
+              }}
+              title={restaurant.name}
+            />
+          ))}
         </MapView>
       )}
 
@@ -70,7 +109,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("ViewProfile")} // Replace 'HomeScreen' with your home screen route name
         style={styles.button}
       >
-        <Text style={styles.buttonText}>View Profile</Text>
+        <Image
+          style={styles.buttonImage}
+          source={require("../assets/profileicon.png")} // replace with your button image path
+        />
       </TouchableOpacity>
 
       <TouchableOpacity
@@ -82,9 +124,23 @@ const HomeScreen = ({ navigation }) => {
           source={require("../assets/jiakIcon.png")} // replace with your button image path
         />
       </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.notificationsButton}
+        onPress={() => setNotificationsEnabled(!notificationsEnabled)}
+      >
+        <Image
+          style={styles.notificationsButtonImage}
+          source={
+            notificationsEnabled
+              ? require("../assets/notifon.png")
+              : require("../assets/notifoff.png")
+          }
+        />
+      </TouchableOpacity>
     </View>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -98,28 +154,46 @@ const styles = StyleSheet.create({
     width: 250,
     height: 250,
   },
-  welcomeText: {
-    color: "#7F2B0F",
-    fontSize: 24,
-    marginBottom: 20,
-  },
+  // welcomeText: {
+  //   color: "#7F2B0F",
+  //   fontSize: 24,
+  //   marginBottom: 0,
+  //   position: "absolute",
+  //   top: 40,
+  // },
   button: {
     position: "absolute",
+    top:20,
+    right:20,
     backgroundColor: "#CD5C5C",
-    paddingVertical: 10,
-    paddingHorizontal: 90,
-    borderRadius: 5,
     marginBottom: 10,
-    borderRadius: 20,
+    borderRadius: 100,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
   },
-  buttonText: {
-    color: "#FFFFFF",
-    fontSize: 18,
+
+  notificationsButton: {
+    position: "absolute",
+    top: 100,
+    right: 30,
+    backgroundColor: "transparent",
+    borderRadius: 100,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
   },
+  notificationsButtonImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  // buttonText: {
+  //   color: "#FFFFFF",
+  //   fontSize: 18,
+  // },
   buttonImage: {
     width: 70, // Set the width of your button image
     height: 70, // Set the height of your button image
@@ -135,8 +209,8 @@ const styles = StyleSheet.create({
   },
   historyButton: {
     position: "absolute", // Position the button over the screen
-    right: 10, // Distance from the left
-    bottom: 10, // Distance from the bottom
+    right: 20, // Distance from the left
+    bottom: 20, // Distance from the bottom
     backgroundColor: "#CD5C5C", // Replace with your desired button color
     borderRadius: 35, // Ensure this is half of the width and height for a perfect circle
     overflow: "hidden", // Ensures the image doesn't bleed outside the border radius
