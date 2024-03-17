9aa76a4106ada74edc7b89a58e3f76fa9c0ab08d
Wei Yang <weiyang35@hotmail.com>
2024-03-17 22:03:58 +0800

    new firebase config

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import { initializeAuth, getAuth, getReactNativePersistence } from '@firebase/au
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzU2iftiwMpeDKDcUTfe2-LfAi067_qE4",
-    authDomain: "sc2006-the-buggers.firebaseapp.com",
-    projectId: "sc2006-the-buggers",
-    storageBucket: "sc2006-the-buggers.appspot.com",
-    messagingSenderId: "1090958684889",
-    appId: "1:1090958684889:web:2f7e01f94ac6b2a21304c8",
-    measurementId: "G-XR3Q3XP179"
-  };
+  apiKey: "AIzaSyCiQQIIUrxtYi-g_vadQQ0dZJitpTSg15g",
+  authDomain: "the-buggers.firebaseapp.com",
+  projectId: "the-buggers",
+  storageBucket: "the-buggers.appspot.com",
+  messagingSenderId: "735219728060",
+  appId: "1:735219728060:web:3acf78d9420d0354ea1ab9"
+
+};
   
   const app = initializeApp(firebaseConfig);
   const auth = initializeAuth(app, {
