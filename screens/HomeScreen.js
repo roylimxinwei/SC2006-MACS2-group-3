96e1c8c7ec5c2d1b85cd3fb5a91adf129afec8e9
roylimxinwei <102473862+roylimxinwei@users.noreply.github.com>
2024-03-20 17:28:05 +0800

    button for on and off

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,32 +2,44 @@
 
 // Welcome screen links to sign up and log in screen
 
-import React, { useState, useEffect, useContext } from "react";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import {
+  Button,
   Dimensions,
   Image,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
-  ScrollView
 } from "react-native";
-import * as Location from "expo-location";
-import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
-import colors from "../config/colors";
-import Header from "./Header";
-import { Switch } from 'react-native-switch';
-import GlobalApi from "../config/GlobalApi";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { Switch } from "react-native-switch";
 import GeoCoding from "../config/GeoCoding";
-import {haversineDistance} from "../config/distanceCalculator";
+import GlobalApi from "../config/GlobalApi";
+import colors from "../config/colors";
+import { haversineDistance } from "../config/distanceCalculator";
+import Header from "./Header";
 
-const screenHeight = Dimensions.get('window').height; 
+const screenHeight = Dimensions.get("window").height;
 
 const HomeScreen = ({ navigation }) => {
   const [currentLocation, setCurrentLocation] = useState(null);
   const [initialRegion, setInitialRegion] = useState(null);
-  const [isEnabled, setIsEnabled] = useState(true);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [distance, setDistance] = useState(null);
+  const [isNotificationVisible, setIsNotificationVisible] = useState(false);
+
+  const NOTIFY_RADIUS_METERS = 500;
+
+  const toggleSwitch = () => {
+    setIsEnabled((previousState) => {
+      const newState = !previousState;
+      console.log("Switch state: " + newState);
+      return newState;
+    });
+  };
 
   const [rawPlacesData, setRawPlacesData] = useState(null);
   const [processedPlaces, setProcessedPlaces] = useState([]);
@@ -52,12 +64,12 @@ const HomeScreen = ({ navigation }) => {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       };
-      
+
       setInitialRegion(newInitialRegion);
       console.log("Initial Region Set: ", newInitialRegion);
     };
 
-      getLocation();
+    getLocation();
   }, []);
 
   useEffect(() => {
@@ -65,16 +77,29 @@ const HomeScreen = ({ navigation }) => {
       const GetNearbyPlace = async () => {
         const location = `${currentLocation.latitude},${currentLocation.longitude}`;
         const radius = 1000; // 1000 meters (1 km)
-        const type = 'restaurant';
+        const type = "restaurant";
 
-        const keywords = ["Japanese", "Korean", "Bakery", "Western", "Chinese", "Indian", "Thai", "Vietnamese", "Malay", "Fast Food"];
+        const keywords = [
+          "Japanese",
+          "Korean",
+          "Bakery",
+          "Western",
+          "Chinese",
+          "Indian",
+          "Thai",
+          "Vietnamese",
+          "Malay",
+          "Fast Food",
+        ];
 
         // Prepare all promises for the API calls
-        const promises = keywords.map(keyword =>
-          GlobalApi.NewNearByPlace(location, radius, type, keyword).catch(error => {
-            console.error('Error fetching nearby places:', error);
-            return null; // Return null or an appropriate value to handle failed requests gracefully
-          })
+        const promises = keywords.map((keyword) =>
+          GlobalApi.NewNearByPlace(location, radius, type, keyword).catch(
+            (error) => {
+              console.error("Error fetching nearby places:", error);
+              return null; // Return null or an appropriate value to handle failed requests gracefully
+            }
+          )
         );
 
         // Wait for all promises to settle
@@ -83,20 +108,25 @@ const HomeScreen = ({ navigation }) => {
         // Process and update state once with all new data
         const allProcessedData = results.flatMap((resp, index) => {
           if (!resp) return []; // Skip processing if the response is null due to an error
-          console.log('Response:', resp);
-          return resp.results.map(place => ({
+          console.log("Response:", resp);
+          return resp.results.map((place) => ({
             latitude: place.geometry.location.lat,
             longitude: place.geometry.location.lng,
             name: place.name,
-            rating: place.rating || 'No rating',
+            rating: place.rating || "No rating",
             cuisine: keywords[index],
             address: place.vicinity,
-            imageUrl: place.photos ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=AIzaSyAOuEs_zxFDQXynk8YZx35_nNWwzpsQy78` : null ,
+            imageUrl: place.photos
+              ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=AIzaSyAOuEs_zxFDQXynk8YZx35_nNWwzpsQy78`
+              : null,
           }));
         });
 
         // Update state once with all new data
-        setProcessedPlaces(prevPlaces => [...prevPlaces, ...allProcessedData]);
+        setProcessedPlaces((prevPlaces) => [
+          ...prevPlaces,
+          ...allProcessedData,
+        ]);
       };
 
       GetNearbyPlace();
@@ -105,25 +135,25 @@ const HomeScreen = ({ navigation }) => {
 
   useEffect(() => {
     if (rawPlacesData && rawPlacesData.results) {
-      const processedData = rawPlacesData.results.map(place => ({
+      const processedData = rawPlacesData.results.map((place) => ({
         latitude: place.geometry.location.lat,
         longitude: place.geometry.location.lng,
         name: place.name,
-        address: place.vicinity
+        address: place.vicinity,
       }));
-      
+
       setProcessedPlaces(processedData); // Update state with processed data
     }
   }, [rawPlacesData]); // This effect depends on rawPlacesData
 
-  useEffect(() => { 
+  useEffect(() => {
     if (currentLocation) {
       GeoCoding.getAddress(currentLocation.latitude, currentLocation.longitude)
-        .then(data => {
+        .then((data) => {
           const address = data.results[0].formatted_address;
           setAddress(address);
         })
-        .catch(error => console.error('Error fetching address:', error));
+        .catch((error) => console.error("Error fetching address:", error));
     }
   }, [currentLocation]);
 
@@ -134,26 +164,24 @@ const HomeScreen = ({ navigation }) => {
       place.latitude,
       place.longitude
     ).toFixed(2); // Round the distance to 2 decimal places
-  
+
     return (
-      <ScrollView 
-        style={styles.restaurantPopUp} 
+      <ScrollView
+        style={styles.restaurantPopUp}
         contentContainerStyle={{ flexGrow: 1 }}
       >
         <View style={styles.contentContainer}>
-          <Image source={{ uri: place.imageUrl}} style={styles.ImageDesign} />
-  
+          <Image source={{ uri: place.imageUrl }} style={styles.ImageDesign} />
+
           <View style={styles.textContainer}>
-            <Text style={styles.calloutTitle}>
-              {place.name}
-            </Text>
+            <Text style={styles.calloutTitle}>{place.name}</Text>
             <Text style={styles.calloutDescription}>
               Rating: {place.rating}
             </Text>
             <Text style={styles.calloutDescription}>
               Cuisine: {place.cuisine}
             </Text>
-            <Text style={styles.calloutDescription}> 
+            <Text style={styles.calloutDescription}>
               Address: {place.address}
             </Text>
             <Text style={styles.calloutDescription}>
@@ -161,11 +189,14 @@ const HomeScreen = ({ navigation }) => {
             </Text>
           </View>
         </View>
-  
-        <TouchableOpacity onPress={() => navigation.navigate("ReviewLandingPage")} style={styles.dismissButtonJiak}> 
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ReviewLandingPage")}
+          style={styles.dismissButtonJiak}
+        >
           <Text style={styles.JiakText}>Jiak!</Text>
         </TouchableOpacity>
-  
+
         <TouchableOpacity onPress={onDismiss} style={styles.dismissButtonClose}>
           <Text style={styles.CloseText}>X</Text>
         </TouchableOpacity>
@@ -176,7 +207,7 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.headerContainer}>
       <View style>
-         <Header/>
+        <Header />
       </View>
       {initialRegion && (
         <MapView style={styles.map} initialRegion={initialRegion}>
@@ -189,22 +220,19 @@ const HomeScreen = ({ navigation }) => {
               title="Your Location"
             >
               <Image
-                source={require('../assets/userIcon.png')}
+                source={require("../assets/userIcon.png")}
                 style={styles.icon}
               />
 
               <Callout>
                 <View style={styles.calloutContainer}>
                   <Text style={styles.calloutTitle}>Your Location</Text>
-                  <Text style={styles.calloutDescription}> 
-                    {address}
-                  </Text>
+                  <Text style={styles.calloutDescription}>{address}</Text>
                 </View>
-              </Callout> 
-
+              </Callout>
             </Marker>
           )}
-          
+
           {processedPlaces.map((place, index) => (
             <Marker
               key={index}
@@ -215,12 +243,12 @@ const HomeScreen = ({ navigation }) => {
               title={place.name}
               onPress={() => setSelectedPlace(place)} // Set the selected place on press
             >
-            <Image
+              <Image
                 source={require("../assets/jiakIcon.png")}
                 style={styles.restaurantIcon}
               />
             </Marker>
-            ))}
+          ))}
         </MapView>
       )}
 
@@ -256,15 +284,15 @@ const HomeScreen = ({ navigation }) => {
         <Switch
           value={isEnabled}
           onValueChange={toggleSwitch}
-          activeText={'Notification On'}
-          inActiveText={'Notification Off'}
+          activeText={"Notification On"}
+          inActiveText={"Notification Off"}
           circleSize={30}
           barHeight={30}
           circleBorderWidth={2}
-          backgroundActive={'green'}
-          backgroundInactive={'#DC4731'}
-          circleActiveColor={'#30a566'}
-          circleInActiveColor={'#DC4731'}
+          backgroundActive={"green"}
+          backgroundInactive={"#DC4731"}
+          circleActiveColor={"#30a566"}
+          circleInActiveColor={"#DC4731"}
           changeValueImmediately={true} // if rendering inside circle, change state immediately or wait for animation to complete
           outerCircleStyle={{}} // style for outer animated circle
           switchLeftPx={1} // denominator for logic when sliding to TRUE position. Higher number = more space from RIGHT of the circle to END of the slider
@@ -272,38 +300,55 @@ const HomeScreen = ({ navigation }) => {
           switchWidthMultiplier={7} // multiplied by the `circleSize` prop to calculate total width of the Switch
           switchBorderRadius={25}
         />
+        {isEnabled && (
+          <SwitchPopup isEnabled={isEnabled} toggleSwitch={toggleSwitch} />
+        )}
       </View>
     </View>
   );
 };
 
+const SwitchPopup = ({ isEnabled, toggleSwitch }) => {
+  return (
+    <View style={styles.popupContainer}>
+      {isEnabled && (
+        <View>
+          <Text style={styles.popupText}>Notifications turned on...</Text>
+          <Text style={styles.popupText}>Looking for places to Jiak!</Text>
+          <Button title="Close" onPress={toggleSwitch} />
+        </View>
+      )}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-  headerContainer:{
-    position:"absolute",
-    width:"100%",
+  headerContainer: {
+    position: "absolute",
+    width: "100%",
   },
 
-  dismissButtonJiak:{
+  dismissButtonJiak: {
     backgroundColor: colors.tertiary,
     position: "absolute",
-    top:125,
-    right:145,
+    top: 125,
+    right: 145,
     paddingBottom: 10,
     paddingTop: 10,
     paddingRight: 35,
     paddingLeft: 35,
     borderRadius: 15,
   },
-  JiakText:{
-    fontSize:16,
+  JiakText: {
+    fontSize: 16,
   },
-  CloseText:{
-    fontSize:16,
+  CloseText: {
+    fontSize: 16,
   },
-  dismissButtonClose:{
+  dismissButtonClose: {
     position: "absolute",
-    top:3,
-    right:-15,
+    top: 3,
+    right: -15,
     paddingBottom: 10,
     paddingTop: 10,
     paddingRight: 35,
@@ -323,8 +368,8 @@ const styles = StyleSheet.create({
   },
   button: {
     position: "absolute",
-    top:60,
-    right:20,
+    top: 60,
+    right: 20,
     backgroundColor: "#CD5C5C",
     marginBottom: 10,
     borderRadius: 100,
@@ -342,20 +387,20 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontSize: 16,
   },
-  switch:{
-    position:"absolute",
-    top:100,
-    left:20,
+  switch: {
+    position: "absolute",
+    top: 100,
+    left: 20,
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
   restaurantIcon: {
     width: 30,
     height: 30,
     borderRadius: 15, // Half of the width and height to make it circular
-    backgroundColor: 'rgba(255, 0, 0, 0.5)', // Red color with 50% opacity
+    backgroundColor: "rgba(255, 0, 0, 0.5)", // Red color with 50% opacity
   },
   historyButton: {
     position: "absolute", // Position the button over the screen
@@ -365,7 +410,7 @@ const styles = StyleSheet.create({
     borderRadius: 35, // Ensure this is half of the width and height for a perfect circle
     overflow: "hidden", // Ensures the image doesn't bleed outside the border radius
   },
-  icon :{
+  icon: {
     width: 40, // Set the width of your image
     height: 40, // Set the height of your image to the same value to maintain aspect ratio
     borderRadius: 20, // Half the width or height to make it round
@@ -387,21 +432,34 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   restaurantPopUp: {
-    position: 'absolute',
-    top: screenHeight * 2 / 3, // Set top to 2/3 of the screen height
+    position: "absolute",
+    top: (screenHeight * 2) / 3, // Set top to 2/3 of the screen height
     left: 0,
     right: 0,
     height: screenHeight / 3,
     backgroundColor: colors.primary,
     zIndex: 2,
-  } ,
+  },
   contentContainer: {
-    flexDirection: 'row', // Align children horizontally
+    flexDirection: "row", // Align children horizontally
     padding: 10, // Add some padding around the content
   },
   textContainer: {
     flex: 1, // Take up remaining space
-    justifyContent: 'center', // Center text vertically
+    justifyContent: "center", // Center text vertically
+  },
+  popupContainer: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    padding: 10,
+    zIndex: 1,
+  },
+  popupText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
   },
 });
 
