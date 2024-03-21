f07c63b32e6aa226c1e4cb3a545ea7522f3f466d
Wei Yang <weiyang35@hotmail.com>
2024-03-21 20:54:48 +0800

    some dining history page change

--- screens/DiningHistoryPage.js
+++ screens/DiningHistoryPage.js
@@ -1,42 +1,70 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { 
   StyleSheet, 
   Text, 
   View, 
   FlatList, 
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
 import {styles} from "../css/DiningHistoryPage_CSS";
 
-// Mock data for the history of dining
-const diningHistoryData = [
-  {
-    id: '1',
-    name: 'ARestaurantName',
-    image: require('../assets/restaurant1_placeholder.jpg'), // replace with actual image path
-    review: 'Best food I had in a century',
-    date: '8/2/2024',
-    rating: 3.5,
-  },
-  {
-    id: '2',
-    name: 'AnotherRestaurantName',
-    image: require('../assets/restaurant2_placeholder.png'), // replace with actual image path
-    review: 'Food is great but Utena chan is greater ~',
-    date: '20/12/2023',
-    rating: 4,
-  },
-  // Add more items here
-];
+import { auth, db, storage} from '../firebase';
+import { doc, setDoc, getDoc, updateDoc} from "firebase/firestore"; 
 
+
+// Mock data for the history of dining
+// const diningHistoryData = [
+//   {
+//     id: '1',
+//     name: 'ARestaurantName',
+//     image: require('../assets/restaurant1_placeholder.jpg'), // replace with actual image path
+//     review: 'Best food I had in a century',
+//     date: '8/2/2024',
+//     rating: 3.5,
+//   },
+//   {
+//     id: '2',
+//     name: 'AnotherRestaurantName',
+//     image: require('../assets/restaurant2_placeholder.png'), // replace with actual image path
+//     review: 'Food is great but Utena chan is greater ~',
+//     date: '20/12/2023',
+//     rating: 4,
+//   },
+//   // Add more items here
+// ];
 
 const DiningHistoryPage = ({navigation}) => {
+
+  const [diningHistoryData, setDiningHistoryData] = useState([]);
+
+  let user = auth.currentUser;
+
+  const fetchData = async () =>{
+  const docRef = doc(db, "diningHistory", user.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+    console.log(docSnap.data().restaurants.length)
+
+
+  } else {
+    // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+  }
+
+}
+
+useEffect(() => {
+  fetchData();
+}, []);
 
   const handleSeeAllReview = () => {
   }
 
   const handleEditReview = () => {
+
     // TBD: delete the original review first
     // then create a new review by navigating to the review page
     navigation.navigate("ReviewPage")
