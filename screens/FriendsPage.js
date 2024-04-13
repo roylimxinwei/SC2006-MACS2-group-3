a7f5f222a2a827b9f24c09b1ce82208cfb10e50c
roylimxinwei <102473862+roylimxinwei@users.noreply.github.com>
2024-04-13 22:27:51 +0800

    removed friend naming feature

--- screens/FriendsPage.js
+++ screens/FriendsPage.js
@@ -7,34 +7,30 @@ import {
   getDocs,
   query,
   setDoc,
+  updateDoc,
   where,
-  updateDoc
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import {
-  Button,
+  Alert,
   Image,
   Keyboard,
   ScrollView,
-  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
-  Alert,
 } from "react-native";
-import { auth, db } from "../firebase";
 import { styles } from "../css/FriendsPage_CSS.js";
-import { set } from "firebase/database";
+import { auth, db } from "../firebase";
 
-const FriendsPage = ({navigation}) => {
+const FriendsPage = ({ navigation }) => {
   const [friendCode, setFriendCode] = useState("");
-  const [friendName, setFriendName] = useState("");
+  //const [friendName, setFriendName] = useState("");
   let [currentUsers, setCurrentUsers] = useState([]);
   let [friends, setFriends] = useState([]);
   let [party, setParty] = useState([]);
-  
 
   let user = auth.currentUser;
   const isFocused = useIsFocused();
@@ -47,13 +43,14 @@ const FriendsPage = ({navigation}) => {
       tempCurrentUsers.push({
         referralCode: doc.data().referralCode,
         userId: doc.id,
+        name: doc.data().name,
       });
     });
 
     setCurrentUsers(tempCurrentUsers);
 
     //https://firebase.google.com/docs/firestore/query-data/queries
-   const tempGuests = await getParty();
+    const tempGuests = await getParty();
 
     //get all your current friends(and check whether they are in a party) and put them in the scrollview.
     let tempFriends = [];
@@ -63,19 +60,19 @@ const FriendsPage = ({navigation}) => {
     );
 
     querySnapshot3.forEach((doc) => {
-       isInParty = false;
+      isInParty = false;
       //check if the friend is also in the party:
-      console.log("length"+tempGuests)
-      if(tempGuests.length > 0){
-      for (let x = 0; x < tempGuests.guests.length; x++) {
-        console.log("guest: "+tempGuests.guests[x])
-        console.log("doc.id: "+doc.id)
-        if (doc.id == tempGuests.guests[x]) {
-          console.log("isinparty true")
-          isInParty = true;
+      //console.log("length"+tempGuests)
+      if (tempGuests.length > 0) {
+        for (let x = 0; x < tempGuests.guests.length; x++) {
+          console.log("guest: " + tempGuests.guests[x]);
+          console.log("doc.id: " + doc.id);
+          if (doc.id == tempGuests.guests[x]) {
+            console.log("isinparty true");
+            isInParty = true;
+          }
         }
-      } 
-    }
+      }
       tempFriends.push({
         amountOwed: doc.data().amountOwed,
         name: doc.data().name,
@@ -83,39 +80,39 @@ const FriendsPage = ({navigation}) => {
         userId: doc.id,
         isInParty: isInParty,
       });
-      
     });
     setFriends(tempFriends);
-    console.log(tempFriends)
+    console.log(tempFriends);
   };
 
-  const getParty = async () =>{
-     //https://firebase.google.com/docs/firestore/query-data/queries
-     let tempParty = [];
-     const ref = collection(db, "party");
-     const q = query(ref, where("host", "==", user.uid));
-     const querySnapshot2 = await getDocs(q);
-     querySnapshot2.forEach((doc) => {
-       // doc.data() is never undefined for query doc snapshots
-       partyDetails = {
-         partyId: doc.id,
-         host: doc.data().host,
-         guests: doc.data().guests,
-         partyMembers: doc.data().groupMembers
-       }
-       tempParty = partyDetails
-       tempParty.length = 1
-     });
-       setParty(tempParty);
-     console.log("party details: "+tempParty.partyId);
-     return tempParty;
-  }
+  const getParty = async () => {
+    //https://firebase.google.com/docs/firestore/query-data/queries
+    let tempParty = [];
+    const ref = collection(db, "party");
+    const q = query(ref, where("host", "==", user.uid));
+    const querySnapshot2 = await getDocs(q);
+    querySnapshot2.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      partyDetails = {
+        partyId: doc.id,
+        host: doc.data().host,
+        guests: doc.data().guests,
+        partyMembers: doc.data().groupMembers,
+      };
+      tempParty = partyDetails;
+      tempParty.length = 1;
+    });
+    setParty(tempParty);
+    console.log("party details: " + tempParty.partyId);
+    return tempParty;
+  };
 
   const addFriend = async () => {
     let codeDoesNotExist = true;
     let friendId = "";
+    let friendName = "";
 
-    if (friendCode.trim().length === 0 || friendName.trim().length === 0) {
+    if (friendCode.trim().length === 0) {
       Alert.alert("Unsuccessful", "Please fill in all fields");
       return;
     }
@@ -126,6 +123,7 @@ const FriendsPage = ({navigation}) => {
       if (friendCode == currentUsers[i].referralCode) {
         codeDoesNotExist = false;
         friendId = currentUsers[i].userId;
+        friendName = currentUsers[i].name;
       }
     }
     if (!codeDoesNotExist) {
@@ -146,72 +144,61 @@ const FriendsPage = ({navigation}) => {
         amountOwed: 0,
       }).then(() => {});
 
-      Alert.alert("Success" , "Friend added");
+      Alert.alert("Success", "Friend added");
       setFriendCode("");
-      setFriendName("");
+      // setFriendName("");
       fetchData();
     } else {
       Alert.alert("Unsuccessful", "Please enter a valid Eater's Code");
     }
   };
   //add someone to your party.
-  const handleAddToParty = async (friend,index) => {
-
+  const handleAddToParty = async (friend, index) => {
     setFriends((prevFriends) => {
-      const newFriends = [...prevFriends];  
+      const newFriends = [...prevFriends];
       newFriends[index].isInParty = true;
       return newFriends;
-  
-    });    
-
+    });
   };
 
-  const handleRemoveFromParty = (friend,index) => {
-
+  const handleRemoveFromParty = (friend, index) => {
     setFriends((prevFriends) => {
-      const newFriends = [...prevFriends];  
+      const newFriends = [...prevFriends];
       newFriends[index].isInParty = false;
       return newFriends;
-  
     });
-  
   };
 
   const confirmParty = async () => {
+    const tempParty = [];
 
-    const tempParty = []
-
-    for(let i = 0;i<friends.length; i++)
-    {
-        if(friends[i].isInParty == true){
-          tempParty.push(friends[i].userId)
-        }
-  
+    for (let i = 0; i < friends.length; i++) {
+      if (friends[i].isInParty == true) {
+        tempParty.push(friends[i].userId);
+      }
     }
 
-    if(party.partyId != null){  
-      console.log("update doc")
+    if (party.partyId != null) {
+      console.log("update doc");
       const updateDocRef = doc(db, "party", party.partyId);
       await updateDoc(updateDocRef, {
         host: user.uid,
-        guests: tempParty
-      }).then(()=>{
-        Alert.alert("Success", "Party Setting Saved!")
+        guests: tempParty,
+      }).then(() => {
+        Alert.alert("Success", "Party Setting Saved!");
         getParty();
       });
-
+    } else {
+      await addDoc(collection(db, "party"), {
+        host: user.uid,
+        guests: tempParty,
+      }).then(() => {
+        Alert.alert("Success", "Party Setting Saved!");
+        getParty();
+      });
     }
-    else{
-    await addDoc(collection(db, "party"), {
-      host: user.uid,
-      guests: tempParty
-    }).then(() => {
-        Alert.alert("Success", "Party Setting Saved!")
-      getParty();
-    });
-  }
-  navigation.navigate('User');
-  }
+    navigation.navigate("User");
+  };
 
   useEffect(() => {
     if (isFocused) {
@@ -219,7 +206,7 @@ const FriendsPage = ({navigation}) => {
     }
   }, [navigation, isFocused]);
 
-  return (  
+  return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={styles.container}>
         <Text style={styles.title}>Add Friend</Text>
@@ -229,72 +216,77 @@ const FriendsPage = ({navigation}) => {
           value={friendCode}
           onChangeText={(text) => setFriendCode(text.trim())}
         />
-        <TextInput
+        {/* <TextInput
           style={styles.input}
           placeholder="Set a name for your friend"
           value={friendName}
           onChangeText={(text) => setFriendName(text.trim())}
-        />
+        /> */}
         <TouchableOpacity onPress={addFriend} style={styles.button3}>
           <Text style={styles.buttonText}>Add Friend</Text>
         </TouchableOpacity>
 
         <Text style={styles.title}>Your Friends</Text>
         {/* ScrollView for friends list */}
-          <ScrollView style={styles.friendsList}>
-            {friends.map((friend, index) => (
-              <TouchableOpacity key={index} style={styles.friendItem}  activeOpacity={1}>
-                {/* <Image source={place.imageUrl} style={styles.ImageDesign} /> */}
-                <View style={styles.userInfo}>
-                  <Image
-                    style={styles.buttonImage}
-                    source={require("../assets/profileicon.png")} // replace with your button image path
-                  />
-                  <View style={styles.nameOwe}>
-                    <Text style={styles.friendUsername}>
-                      Name: {friend.name}
-                    </Text>
-                    <Text style={styles.friendUsername}>
-                      Amount Owed: ${friend.amountOwed}
-                    </Text>
-                  </View>
+        <ScrollView style={styles.friendsList}>
+          {friends.map((friend, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.friendItem}
+              activeOpacity={1}
+            >
+              {/* <Image source={place.imageUrl} style={styles.ImageDesign} /> */}
+              <View style={styles.userInfo}>
+                <Image
+                  style={styles.buttonImage}
+                  source={require("../assets/profileicon.png")} // replace with your button image path
+                />
+                <View style={styles.nameOwe}>
+                  <Text style={styles.friendUsername}>Name: {friend.name}</Text>
+                  <Text style={styles.friendUsername}>
+                    Amount Owed: ${friend.amountOwed}
+                  </Text>
                 </View>
+              </View>
 
-                {/* If friend is not in party with user, allow them to add friend party. */}
-                {!friend.isInParty && (
-                  <TouchableOpacity
-                    style={styles.buttonInParty}
-                    onPress={() => handleAddToParty(friend,index)}
-                  >
-                    <Text style={styles.buttonText}>Add to Party {friend.isInParty}</Text>
-                  </TouchableOpacity>
-                )}
+              {/* If friend is not in party with user, allow them to add friend party. */}
+              {!friend.isInParty && (
+                <TouchableOpacity
+                  style={styles.buttonInParty}
+                  onPress={() => handleAddToParty(friend, index)}
+                >
+                  <Text style={styles.buttonText}>
+                    Add to Party {friend.isInParty}
+                  </Text>
+                </TouchableOpacity>
+              )}
 
-                {/* If friend is in party with user, allow them to remove friend from party. */}
-                {friend.isInParty && (
-                  <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => handleRemoveFromParty(friend,index)}
-                  >
-                    <Text style={styles.buttonText}>Remove from Party {friend.isInParty}</Text>
-                  </TouchableOpacity>
-                )}
-              </TouchableOpacity>
-            ))}
-            {/* <Text style={styles.pad}>for padding</Text> */}
-          </ScrollView>
-        
+              {/* If friend is in party with user, allow them to remove friend from party. */}
+              {friend.isInParty && (
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleRemoveFromParty(friend, index)}
+                >
+                  <Text style={styles.buttonText}>
+                    Remove from Party {friend.isInParty}
+                  </Text>
+                </TouchableOpacity>
+              )}
+            </TouchableOpacity>
+          ))}
+          {/* <Text style={styles.pad}>for padding</Text> */}
+        </ScrollView>
+
+        <TouchableOpacity style={styles.button2} onPress={() => confirmParty()}>
+          <Text style={styles.buttonText2}>Confirm Party</Text>
+        </TouchableOpacity>
         <TouchableOpacity
-                    style={styles.button2}
-                    onPress={() => confirmParty()}
-                  >
-                    <Text style={styles.buttonText2}>Confirm Party</Text>
-                  </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('User')} style={styles.backButton}>
-        <Text style={styles.backButtonText}>X</Text>
-      </TouchableOpacity>
+          onPress={() => navigation.navigate("User")}
+          style={styles.backButton}
+        >
+          <Text style={styles.backButtonText}>X</Text>
+        </TouchableOpacity>
       </View>
-
     </TouchableWithoutFeedback>
   );
 };
